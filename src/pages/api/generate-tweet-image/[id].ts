41b2b92d82d6ae2641b8fb580a8d16a5a011eaf9
Matheus Trindade <trindadematheus27@gmail.com>
2023-08-09 21:28:29 -0300

    base

---
+++ src/pages/api/generate-tweet-image/[id].ts
@@ -0,0 +1,20 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import { getTweet } from "react-tweet/api";
+
+const handler = async (req: VercelRequest, res: VercelResponse) => {
+  const tweetId = req.query.id;
+
+  if (req.method !== "GET" || typeof tweetId !== "string") {
+    res.status(400).json({ error: "Bad Request." });
+    return;
+  }
+
+  try {
+    const tweet = await getTweet(tweetId);
+    res.status(tweet ? 200 : 404).json({ data: tweet ?? null });
+  } catch (error: any) {
+    res.status(400).json({ error: error.message ?? "Bad request." });
+  }
+};
+
+export default handler;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,121 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { FormEvent, useRef, useState } from "react";
+import { Tweet } from "react-tweet/api";
+import axios from "axios";
+import downloadjs from "downloadjs";
+import html2canvas from "html2canvas";
+import { toast } from "react-hot-toast";
 
-const inter = Inter({ subsets: ['latin'] })
+// @ts-ignore
+import getTweetId from "get-tweet-id";
 
 export default function Home() {
+  const [tweetData, setTweetData] = useState<Tweet | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const imageRef = useRef<HTMLDivElement>(null);
+
+  async function handleGetTweetData(evt: FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+
+    if (inputRef.current) {
+      setIsLoading(true);
+
+      try {
+        const { data } = await axios.get(
+          `/api/generate-tweet-image/${getTweetId(inputRef.current.value)}`
+        );
+
+        setTweetData(data.data);
+        inputRef.current.value = "";
+      } catch (error) {
+        toast.error("Oops, try again!");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  }
+
+  function handleDownload() {
+    if (imageRef && tweetData) {
+      html2canvas(document.getElementById("image")!, { useCORS: true }).then(
+        function (canvas) {
+          const dataURL = canvas.toDataURL("image/png");
+
+          downloadjs(
+            dataURL,
+            `tweet-to-image-${tweetData.id_str}.png`,
+            "image/png"
+          );
+
+          toast.success("Image downloaded with success!");
+        }
+      );
+    }
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className="container max-w-4xl">
+        <h1 className="text-4xl font-bold pt-8 text-white">Tweet to Post</h1>
+
+        <form className="mb-12 pt-4" onSubmit={handleGetTweetData}>
+          <div className="flex items-end gap-4">
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Tweet URL</span>
+              </label>
+              <input
+                ref={inputRef}
+                type="text"
+                placeholder="Type here"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </div>
+
+            <button
+              className={`btn btn-primary ${isLoading && "loading disabled"}`}
+            >
+              Generate
+            </button>
+          </div>
+        </form>
+
+        {tweetData && (
+          <div>
+            <div
+              ref={imageRef}
+              id="image"
+              className="bg-black w-[600px] h-[600px] flex flex-col justify-center px-20 items-start"
+            >
+              <div className="flex items-center gap-4">
+                <img
+                  className="rounded-full w-14 h-14"
+                  src={tweetData.user.profile_image_url_https}
+                  alt={`${tweetData.user.name} profile pic`}
+                />
+
+                <div>
+                  <h1 className="text-white">{tweetData.user.name}</h1>
+                  <p className="text-slate-400">
+                    @{tweetData.user.screen_name}
+                  </p>
+                </div>
+              </div>
+
+              <p className="text-slate-200 mt-6">{tweetData.text}</p>
+            </div>
+
+            <button
+              type="button"
+              onClick={handleDownload}
+              className="btn btn-accent mt-4"
+            >
+              download
+            </button>
+          </div>
+        )}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
